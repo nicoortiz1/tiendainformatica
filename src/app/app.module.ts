08111dad9959f740961fb1e68381f400614ba416
nicoortiz1 <nicoortizpe@gmail.com>
2023-05-23 15:49:15 -0300

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './producto/producto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/producto/producto.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos',
+  templateUrl: './producto.component.html',
+  styleUrls: ['./producto.component.css']
+})
+export class ProductosComponent {
+  productos = [
+    { nombre: 'PC', precio: 300000, descuento: '20%', imagen: 'assets/images/pc.jpg' },
+    { nombre: 'Teclado', precio: 1000, descuento: '10%', imagen: 'assets/images/teclado.jpg' },
+    { nombre: 'Monitor', precio: 150000, descuento: '5%', imagen: 'assets/images/monitor.jpg' },
+    { nombre: 'Mouse', precio: 3500 , descuento: '15%', imagen: 'assets/images/mouse.jpg' },
+    { nombre: 'Impresora', precio: 50000, descuento: '25%', imagen: 'assets/images/impresora.jpg' },
+    { nombre: 'Procesador', precio: 100000, descuento: '', imagen: 'assets/images/procesador.jpg' },
+    { nombre: 'Auriculares', precio: 50000, descuento: '25%', imagen: 'assets/images/auriculares.jpg' },
+    { nombre: 'Memoria RAM', precio: 100000, descuento: '5%', imagen: 'assets/images/ram.jpg' }
+  ];
+ 
+  descuentoBusqueda: string =''; // Variable para almacenar el valor de búsqueda
+  
+  buscarPorDescuento() {
+    if (this.descuentoBusqueda) {
+      this.productos = this.productos.filter(producto =>
+        parseInt(producto.descuento) >= parseInt(this.descuentoBusqueda)
+      );
+    }
+    else {
+      this.productos = [
+        { nombre: 'PC', precio: 300000, descuento: '20%', imagen: 'assets/images/pc.jpg' },
+        { nombre: 'Teclado', precio: 1000, descuento: '10%', imagen: 'assets/images/teclado.jpg' },
+        { nombre: 'Monitor', precio: 150000, descuento: '5%', imagen: 'assets/images/monitor.jpg' },
+        { nombre: 'Mouse', precio: 3500 , descuento: '15%', imagen: 'assets/images/mouse.jpg' },
+        { nombre: 'Impresora', precio: 50000, descuento: '25%', imagen: 'assets/images/impresora.jpg' },
+        { nombre: 'Procesador', precio: 100000, descuento: '', imagen: 'assets/images/procesador.jpg' },
+        { nombre: 'Auriculares', precio: 50000, descuento: '25%', imagen: 'assets/images/auriculares.jpg' },
+        { nombre: 'Memoria RAM', precio: 100000, descuento: '5%', imagen: 'assets/images/ram.jpg' }
+      ];
+    }
+  }
+  
+  
+}
